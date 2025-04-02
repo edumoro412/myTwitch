84dae46787008bb90c7ebc30bd943475836fd0c3
edumoro412 <edumoro2003@gmail.com>
2025-04-02 15:47:41 +0200

    feat(layout): update layout structure to include navigation component and improve styling

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,22 @@
 <script setup lang="ts">
-import Header from '~/components/Header.vue';
+import Header from '~/components/layout/HeaderComponent.vue';
+import NavComponent from '~/components/layout/NavComponent.vue';
 </script>
 <template>
   <div>
-    <Header></Header>
+    <Header />
+    <main class="main">
+      <NavComponent />
+      <section>
+        <h2>Aqui va el slot</h2>
+        <slot />
+      </section>
+    </main>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped lang="scss">
+.main {
+  @include flex($justify-content: left);
+}
+</style>
