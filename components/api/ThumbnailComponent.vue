f0da70a0cb62647e7134ed919543ed60009dc303
edumoro412 <edumoro2003@gmail.com>
2025-04-04 14:21:22 +0200

    feat(components): add profile picture display in ThumbnailComponent

--- components/api/ThumbnailComponent.vue
+++ components/api/ThumbnailComponent.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
+import { computed } from 'vue';
 import userIconRed from '../icons/userIconRed.vue';
+
 interface Stream {
   game_id: string;
   game_name: string;
@@ -18,6 +20,12 @@ const thumbnailUrl = computed(() => {
     ?.replace('{width}', '450')
     ?.replace('{height}', '240');
 });
+
+const { data: profilePic } = await useFetch('/api/twitch-profile-picture', {
+  query: { id: props.user_id },
+});
+
+const pictureUrl = profilePic.value?.data[0]?.profile_image_url;
 </script>
 
 <template>
@@ -29,14 +37,22 @@ const thumbnailUrl = computed(() => {
         class="thumbnail-image"
         loading="lazy"
       />
-      <div class="stream__viewer">
-        <userIconRed class="stream__viewer--usericon" />
-        <span class="count">{{ props.viewer_count }}</span>
-      </div>
+    </div>
+    <div class="stream__viewer">
+      <userIconRed class="stream__viewer--usericon" />
+      <span class="count">{{ props.viewer_count }}</span>
     </div>
     <div class="stream__info">
+      <div class="stream__info-header">
+        <img
+          v-if="pictureUrl"
+          :src="pictureUrl"
+          :alt="`Profile picture of ${props.user_name}`"
+          class="stream__profile-pic"
+        />
+        <h3 class="stream__info--streamername">{{ props.user_name }}</h3>
+      </div>
       <h3 class="stream__info--title">{{ props.title }}</h3>
-      <p class="stream__info--streamername">{{ props.user_name }}</p>
       <p class="stream__info--category">{{ props.game_name }}</p>
     </div>
   </div>
@@ -46,27 +62,55 @@ const thumbnailUrl = computed(() => {
 .stream {
   width: min-content;
 
+  &__thumbnail {
+    position: relative;
+  }
+
   &__viewer {
     font-weight: bold;
     font-size: 1em;
     color: rgb(255, 2, 2);
-    @include flex($justify-content: left, $flex-grow: 0);
+    display: flex;
+    align-items: center;
+    margin: 0.7em 0 1em 0;
+
     &--usericon {
+      margin-right: 5px;
       @include flex($justify-content: left, $flex-grow: 0);
     }
   }
+
   &__info {
     color: white;
-    &--title {
-      font-size: 1.2em;
-      margin-bottom: 1em;
+
+    &-header {
+      display: flex;
+      align-items: center;
+      margin-bottom: 0.5em;
     }
 
-    &--streamername,
-    &--category {
+    &--streamername {
+      font-size: 1.2em;
+      margin: 0;
+    }
+
+    &--title {
       font-size: 1em;
+      margin-bottom: 0.5em;
+    }
+
+    &--category {
+      font-size: 0.9em;
       color: #7b7b8b;
     }
+  }
+
+  &__profile-pic {
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    margin-right: 10px;
+    object-fit: cover;
   }
 }
 </style>
