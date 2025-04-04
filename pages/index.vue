a9217098f74bcd94c1e32af27776ef27a50d8091
edumoro412 <edumoro2003@gmail.com>
2025-04-04 10:08:45 +0200

    feat(pages): integrate ThumbnailComponent for improved Twitch stream display

--- pages/index.vue
+++ pages/index.vue
@@ -1,75 +1,51 @@
 <script setup lang="ts">
-// Cargar config
-const config = useRuntimeConfig();
-const cliente_id = config.public.twitchClientId;
+import ThumbnailComponent from '~/components/api/ThumbnailComponent.vue';
 
 const { data: streamsData } = await useFetch('/api/twitch-top-streams');
-console.log('ESTO ES LO DEL DATA', streamsData);
-const AhoraDirectos = streamsData.value?.data || []; // Array vacío si no hay datos
 
-// Obtener la miniatura del primer stream (o el que necesites)
-const miniatura = AhoraDirectos[0]?.thumbnail_url
-  ?.replace('{width}', '640')
-  .replace('{height}', '360');
+const AhoraDirectos = streamsData?.value?.data || [];
 </script>
 
 <template>
-  <div>
+  <div class="main">
+    <p class="main__title">
+      <span class="main__title--blue">Live channels</span> we think you´ll like
+    </p>
     <h1>Videos de Twitch</h1>
-
-    <!-- Mostrar miniatura del stream -->
-    <img v-if="miniatura" :src="miniatura" alt="Miniatura del directo" />
-
-    <div v-if="AhoraDirectos.length > 0">
-      <div
+    <section class="main__streams">
+      <article
         v-for="stream in AhoraDirectos"
-        :key="stream.user_name || stream.id"
-        class="mb-10 flex gap-4"
+        :key="stream.id"
+        class="main__streans--article"
       >
-        <div>
-          <h2>{{ stream.user_name || stream.user_id }}</h2>
-
-          <img
-            :src="
-              stream.thumbnail_url
-                .replace('{width}', '640')
-                .replace('{height}', '360')
-            "
-            :alt="`Miniatura de ${stream.user_name}`"
-          />
-
-          <iframe
-            width="560"
-            height="315"
-            :src="`https://player.twitch.tv/?channel=${stream.user_name}&parent=localhost`"
-            frameborder="0"
-            allowfullscreen
-          ></iframe>
-        </div>
-
-        <div>
-          <h3>Chat</h3>
-          <iframe
-            width="350"
-            height="315"
-            :src="`https://www.twitch.tv/embed/${stream.user_name}/chat?parent=localhost`"
-            frameborder="0"
-            scrolling="no"
-          ></iframe>
-        </div>
-      </div>
-    </div>
-
-    <div v-else>
-      <p>No hay streams disponibles en este momento.</p>
-    </div>
-
-    <p>cliente_id -> {{ cliente_id }}</p>
+        <ThumbnailComponent v-bind="stream" />
+      </article>
+    </section>
   </div>
 </template>
 
-<style>
-p {
-  color: white;
+<style lang="scss" scoped>
+.main {
+  &__title {
+    padding-top: 1em;
+    font-size: 1.3em;
+    color: white;
+    font-weight: bold;
+
+    &--blue {
+      color: rgb(38, 152, 239);
+    }
+  }
+
+  &__streams {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-template-rows: repeat(3, 1fr);
+    gap: 4em;
+
+    &--article {
+      grid-row-start: 2;
+    }
+  }
 }
 </style>
