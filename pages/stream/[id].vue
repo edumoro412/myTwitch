ca37e052c49523c1f8e4abbb3e7242e4163d0045
edumoro412 <edumoro2003@gmail.com>
2025-04-05 16:39:48 +0200

    feat(pages): refactor Twitch stream page to use user_name variable for iframe sources

--- pages/stream/[id].vue
+++ pages/stream/[id].vue
@@ -11,17 +11,28 @@ const { data } = await useFetch('/api/twitch-user-info', {
 });
 
 console.log('aqui esta la da', data);
-console.log('===============', data.value?.data[0].user_name);
+const user_name = data.value?.data[0].user_name.toString();
+console.log('===============', user_name);
 </script>
 
 <template>
   <div>
+    <h1>Directo</h1>
     <iframe
       width="560"
       height="315"
-      :src="`https://player.twitch.tv/?channel=${data.value?.data[0].user_name}&parent=localhost`"
+      :src="`https://player.twitch.tv/?channel=${user_name}&parent=localhost`"
       frameborder="0"
       allowfullscreen
+    ></iframe>
+
+    <h3>Chat</h3>
+    <iframe
+      width="350"
+      height="315"
+      :src="`https://www.twitch.tv/embed/${user_name}/chat?parent=localhost`"
+      frameborder="0"
+      scrolling="no"
     ></iframe>
   </div>
 </template>
